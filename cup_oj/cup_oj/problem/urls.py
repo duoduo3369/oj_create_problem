23fda629efc290b539474ae1299b79a289a23fa6
duoduo <duoduo3_69@163.com>
2012-05-05 18:56:12 +0800

    configtable done

--- cup_oj/cup_oj/problem/urls.py
+++ cup_oj/cup_oj/problem/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^problem_meta_upload/$', problem_meta_upload, name='problem_meta_upload'), 
-    url(r'^keyword_checkconfig_upload/$', keyword_checkconfig_upload, name='keyword_checkconfig_upload'),                
+    url(r'^keyword_checkconfig_upload/$', keyword_checkconfig_upload, name='keyword_checkconfig_upload'),
+    url(r'^compile_config_upload/$', compile_config_upload, name='compile_config_upload'),                
     url(r'^submit_success/$', submit_success, name = 'submit_success'),
 )
--- cup_oj/cup_oj/problem/views.py
+++ cup_oj/cup_oj/problem/views.py
@@ -1,31 +1,24 @@
 from django.shortcuts import render_to_response
-from cup_oj.problem.forms import ProblemMetaForm, KeywordCheckConfigForm
+from cup_oj.problem.forms import CompileConfigForm, KeywordCheckConfigForm, ProblemMetaForm
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, QueryDict
 from django.template.response import TemplateResponse
 from django.template import RequestContext
 
-def get_classoject_id(obj):
-    value = []
-    queryset = obj.objects.all()
-    for v in queryset:
-        value.append(v.id)
-    return value
-    
 def submit_success(request):
 	return render_to_response('problem/submit_success.html')
 								 
-def problem_meta_upload(request):
+def compile_config_upload(request):
     if request.method == 'POST':
-        form = ProblemMetaForm(request.POST)
+        form = CompileConfigForm(request.POST)
         
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('submit_success'))
     else:
-        form = ProblemMetaForm()
-        
-    return render_to_response("problem/problem_meta_upload.html", {
+        form = CompileConfigForm()    
+    
+    return render_to_response("problem/upload.html", {
         'form': form,}, context_instance=RequestContext(request)) 
         
 def keyword_checkconfig_upload(request):
@@ -38,5 +31,19 @@ def keyword_checkconfig_upload(request):
     else:
         form = KeywordCheckConfigForm()
         
-    return render_to_response("problem/keyword_checkconfig_upload.html", {
+    return render_to_response("upload.html", {
         'form': form,}, context_instance=RequestContext(request)) 
+        
+def problem_meta_upload(request):
+    if request.method == 'POST':
+        form = ProblemMetaForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('submit_success'))
+    else:
+        form = ProblemMetaForm()    
+    
+    return render_to_response("problem/upload.html", {
+        'form': form,}, context_instance=RequestContext(request))     
+
